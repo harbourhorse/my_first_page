862630282051b4e45f497b9706cd0315283c4456
iMac <imac@iMac-iMac.local>
2016-06-06 11:41:23 +0200

    Final Refractor

--- REAMDE.md
+++ REAMDE.md
@@ -1,1 +1,9 @@
-This is my first page. Konrad Slazyk
+# This is my first page. Konrad Slazyk
+
+clone repo and `cd ~/my_first_page/`
+
+then run `bundle` to install all dependencies
+
+finally run `rails s` to start the server on `localhost:3000`
+
+
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,94 +1,82 @@
 function toggleMenu() {
-	$('.navLink1').hover(function(){
-    	$(this).find('.dropDownMenu li').stop().slideToggle(450);
+  $('.mainMenu__link--dropdown').hover(function(){
+      $('.dropDownMenu').toggleClass('dropDownMenu--visible');
     });
 };
 
 function stickyNavbar() {
-	var navOffset = $('.navMenu').offset().top;
+  var navOffset = $('.navMenu').offset().top;
 
-	$(window).scroll(function() {
-	var scrollPos = $(window).scrollTop();
-
-		if (scrollPos >= navOffset) {
-			$('body').addClass('navbar-fixed');
-		} else {
-			$('body').removeClass('navbar-fixed');
-		};
-	});
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop();
+    $('body').toggleClass('navbar-fixed', scrollPos >= navOffset);
+  });
 };
 
 function activeSite() {
-	$('.navMenu a').each(function() {
-    if ($(this).prop('href') == window.location.href) {
-      $(this).addClass('current');
-  	};
-	});
+  $('.mainMenu__link, .dropDownMenu__item').each(function() {
+      if ($(this).prop('href') == window.location.href) {
+        $(this).addClass('current');
+      };
+  });
 };
 
-function slideDownMenu() {
-	$('.mainContent, .mainContentImg').slideDown(700);
+function slideDownContent() {
+  $('.mainContent, .mainContentImg').hide().slideDown(700);
 };
-    
+
 function animateText() {
-	$('.animateText').textillate({
-		in: { effect: 'flipInX', delay:1.5},
-	});
+  $('.animateText').textillate({
+    in: { effect: 'flipInX', delay:1.5},
+  });
 };
 
 function animateLine() {
-	$('.line').hide().animate({width: 'toggle'}, 1000);
+  $('.line').hide().animate({width: 'toggle'}, 1000);
 };
 
 function footerClock() {
-	var clock = new Date();
-	var hour = clock.getHours();
-		if (hour < 10)
-			hour = '0' + hour;
-		var minute = clock.getMinutes();
-			if (minute < 10)
-				minute = '0' + minute;
-		var second = clock.getSeconds();
-			if (second < 10)
-				second = '0' + second;
+  var clock = new Date();
+  var hour = clock.getHours();
+  if (hour < 10) { hour = '0' + hour };
+  var minute = clock.getMinutes();
+  if (minute < 10) { minute = '0' + minute };
+  var second = clock.getSeconds();
+  if (second < 10) { second = '0' + second };
 
-document.getElementById('clock').innerHTML = hour + ':' + minute + ':' + second;
+  $('#clock').html(hour + ':' + minute + ':' + second);
 
-setTimeout('footerClock()', 1000);
 };
 
 function copyrightDate() {
-	var today = new Date();
-	var year = today.getFullYear();
+  var today = new Date();
+  var year = today.getFullYear();
 
-document.getElementById('copyright').innerHTML = '<p>Copyright © Konrad Slazyk ' + year + '</p>' ;
+  $('#copyright').html('<p>Copyright © Konrad Slazyk ' + year + '</p>');
 };
 
 function imgLightbox() {
-	$('.lightbox').click(function(event){
-		event.preventDefault();
-		var src = $(event.target).attr('src');
-			$('.backdrop, .box').animate({'opacity':'.50'}, 300, 'linear');
-			$('.box').animate({'opacity':'1.00'}, 200, 'linear');
-			$('.backdrop, .box').css('display', 'block');
-			$('.box').html('<img src="' + src + '" />');
-	});
-	$('.backdrop').click(function(){
-		$('.backdrop, .box').animate({'opacity':'0'}, 300, 'linear', function(){
-			$('.backdrop, .box').css('display', 'none');
-		});
-	});
+  $('.lightbox').click(function(event){
+    event.preventDefault();
+    var src = $(event.target).attr('src');
+    $('.backdrop, .box').addClass('visible');
+    $('.box img').attr('src', src);
+  });
+
+  $('.backdrop').click(function(event){
+    event.preventDefault();
+    $('.backdrop, .box').removeClass('visible');
+  });
 };
 
-
 $(document).ready(function() {
-	toggleMenu();
-	stickyNavbar();
-	slideDownMenu();
-	animateText();
-	animateLine();
-	footerClock();
-	copyrightDate();
-	imgLightbox();
-	activeSite();
+  toggleMenu();
+  stickyNavbar();
+  slideDownContent();
+  animateText();
+  animateLine();
+  setInterval(footerClock, 1000);
+  copyrightDate();
+  imgLightbox();
+  activeSite();
 });
