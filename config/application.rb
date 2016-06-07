e19820d7a10514d18c34fa6fdd3cc151e2e938bf
iMac <imac@iMac-iMac.local>
2016-06-07 13:18:05 +0200

    heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module MyFirstPage
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
 
-config.serve_static_assets = true
+
