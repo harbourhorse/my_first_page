522c1a6a16344df3d634c81d4ab2aadc8ccccadb
iMac <imac@iMac-iMac.local>
2016-06-07 13:09:37 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.precompile += %w( timeline.js )
   # Compress JavaScripts and CSS.
-  config.assets.compress = true
+  config.assets.compress = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
